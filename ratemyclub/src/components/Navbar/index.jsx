8e33a44558060bc6b214f08148f799790842794f
Selena Li <selenali@live.unc.edu>
2022-04-19 23:25:59 -0400

    wip: Create Club Page

--- ratemyclub/src/components/Navbar/index.jsx
+++ ratemyclub/src/components/Navbar/index.jsx
@@ -1,62 +1,81 @@
-import React, {useState} from 'react'
-import "./Modal.scss"
+import React, { useState } from "react";
+import "./Modal.scss";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <>
-        {isOpen && <Modal isOpen ={setIsOpen}/>}
-        <nav>
-       {/*<img src="./img/logo.png" alt="" />*/}
+  return (
+    <>
+      {isOpen && <Modal isOpen={setIsOpen} />}
+      <nav>
+        <a href="../">
+          <img src="./img/logo-header.png" alt="" />
+        </a>
         <ul>
-          <li><a href="">Categories</a></li>
-          <li><a href="./create">Club Organizer?</a></li>
+          <li>
+            <a href="">Categories</a>
+          </li>
+          <li>
+            <a href="./create">Club Organizer?</a>
+          </li>
         </ul>
-        <button className="btn btn-secondary" onClick={()=>{setIsOpen(true);}}>Log In</button>
+        <button
+          className="btn btn-secondary"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          Log In
+        </button>
         <button className="btn btn-primary">Sign Up</button>
       </nav>
-      </>
-    );
+    </>
+  );
 };
 
-const suModal = ({isOpen}) => {
-    return (
-        <div className="modalBackground">
-            <div className="modalContainer">
-                <button className ="close" onClick={()=>isOpen(false)}><i class="fa fa-times" aria-hidden="true"></i></button>
-                <h1>Sign Up</h1>
-                <label for="email">Email</label>
-                <input type = "email" placeholder="Email" />
-                <label for="password">Password</label>
-                <input type = "password" placeholder="password" />
-                <button className="btn btn-primary">Continue</button>
-                <div>
-                    <p>Dont have an account yet?</p>
-                    <a href="" onClick={()=>isOpen(false)}>Sign Up</a>
-                </div>
-            </div>
+const suModal = ({ isOpen }) => {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <button className="close" onClick={() => isOpen(false)}>
+          <i className="fa fa-times" aria-hidden="true"></i>
+        </button>
+        <h1>Sign Up</h1>
+        <label for="email">Email</label>
+        <input type="email" placeholder="Email" />
+        <label for="password">Password</label>
+        <input type="password" placeholder="password" />
+        <button className="btn btn-primary">Continue</button>
+        <div>
+          <p>Dont have an account yet?</p>
+          <a href="" onClick={() => isOpen(false)}>
+            Sign Up
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
-const Modal = ({isOpen}) => {
-    return (
-        <div className="modalBackground">
-            <div className="modalContainer">
-                <button className ="close" onClick={()=>isOpen(false)}><i class="fa fa-times" aria-hidden="true"></i></button>
-                <h1>Log In</h1>
-                <label for="email">Email</label>
-                <input type = "email" placeholder="Email" />
-                <label for="password">Password</label>
-                <input type = "password" placeholder="password" />
-                <button className="btn btn-primary">Continue</button>
-                <div>
-                    <p>Dont have an account yet?</p>
-                    <a href="">Sign Up!</a>
-                </div>
-            </div>
+const Modal = ({ isOpen }) => {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <button className="close" onClick={() => isOpen(false)}>
+          <i className="fa fa-times" aria-hidden="true"></i>
+        </button>
+        <h1>Log In</h1>
+        <label for="email">Email</label>
+        <input type="email" placeholder="Email" />
+        <label for="password">Password</label>
+        <input type="password" placeholder="password" />
+        <button className="btn btn-primary">Continue</button>
+        <div>
+          <p>Dont have an account yet?</p>
+          <a href="">Sign Up!</a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- ratemyclub/src/pages/createclub.jsx
+++ ratemyclub/src/pages/createclub.jsx
@@ -6,8 +6,132 @@ const CreateClub = () => {
   return (
     <>
       <Navbar />
+      <main id="create" className="py-4">
+        <section className="container">
+          <div className="intro">
+            <h1>Create your club!</h1>
+            <p>
+              Are you an executive member of a UNC club? Do you want to promote
+              your club to reach it’s highest potential? Looking for more
+              members to keep the club exciting? Rate my club! allows you as a
+              club leader to add
+            </p>
+          </div>
+          <section className="create-form">
+            {/*  <form onSubmit={this.handleSubmit}>*/}
+            <div className="row">
+              <div className="col">
+                <label>Club Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Club Name"
+                  aria-label="First name"
+                />
+              </div>
+              <div className="col">
+                <label for="categoryInput">Club Category</label>
+                <select name="category" id="" className="form-select">
+                  <option selected>Select Category</option>
+                  <option value="Sports">Sports</option>
+                  <option value="Academic">Academic</option>
+                  <option value="Greek">Greek</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="w-75">
+              <label for="formFile" className="form-label">
+                Upload club image
+              </label>
+              <input className="form-control" type="file" id="formFile" />
+            </div>
+
+            <div className="row">
+              <strong>Contact Information</strong>
+              <div className="col socials">
+                <i class="fa-solid fa-envelope-open-text"></i>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Club Email"
+                  aria-label="Email"
+                />
+              </div>
+              <div className="col socials">
+                <i class="fa-solid fa-code"></i>{" "}
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Website"
+                  aria-label="Website"
+                />
+              </div>
+            </div>
+            <div className="row mt-3">
+              <div className="col socials">
+              <i class="fa-brands fa-facebook-square"></i>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Facebook Handle"
+                  aria-label="Facebook"
+                />
+              </div>
+              <div className="col socials">
+                <i class="fa-brands fa-instagram-square"></i>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Instagram Handle"
+                  aria-label="Instagram"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="form-outline w-100">
+                <label className="form-label" for="textArea">
+                  Club Description
+                </label>
+                <textarea
+                  className="form-control"
+                  id="textArea"
+                  rows="5"
+                ></textarea>
+              </div>
+            </div>
+            {/*</form>*/}
+          </section>
+        </section>
+      </main>
     </>
   );
 };
 
 export default CreateClub;
+
+{
+  /*
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                  className="form-control"
+                  id="nameImput"
+                  placeholder="Club Name"
+                />
+              </div>
+              <div className="form-group">
+                <label for="categoryImput">Club Category</label>
+                <input
+                  name="text"
+                  type="category"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                  className="form-control"
+                  id="emailImput"
+                  placeholder="Club Category"
+                />
+*/
+}
