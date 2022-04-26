d365259d66e206b4d0b2dba39df424ee154c4d5f
averyc997 <59751114+averyc997@users.noreply.github.com>
2022-04-25 20:56:35 -0400

    fixed club page and review modal

--- ratemyclub/src/pages/club.jsx
+++ ratemyclub/src/pages/club.jsx
@@ -5,64 +5,74 @@ import { Dialog } from "@material-ui/core";
 import "./Modal.scss";
 
 const Club = () => {
-
   const [reviewOpen, setReviewOpen] = useState(false);
-
   const handleReview = (event) => {
     setReviewOpen(true);
   };
-
-    return (
-        <>
-          <Navbar />
-          <main id="club" className="py-4">
-            <section className="container">
-              <div className="club">
-                <div className="clubLeft">
-                  <div className="row">
-                    <img className="clubImg"></img>
-                    <h1 className="clubTitle">Completely Fake Club</h1>
-                    <img className="share"></img>
-                  </div>
-                  <div className="row">
-                    <img className="fb"></img>
-                    <img className="email"></img>
-                    <img className="insta"></img>
-                    <img className="website"></img>
-                  </div>
-                  <div className="row">
-                    <div className="tags">
-                      <p>tag</p>
-                    </div>
-                    <div className="tags">
-                      <p>tag</p>
-                    </div>
-                  </div>
-                  <p className="clubDescrip">A club description.</p>
-                  <button className="rateClub" onClick={handleReview}>Rate this club!</button>
+  return (
+    <>
+      <Navbar />
+      <main id="club" className="py-4">
+        <section className="container">
+          <div className="row club">
+            <div className="col clubLeft">
+              <div className="row">
+                <div className="col">
+                  <img className="clubImg"></img>
                 </div>
-                <reviewDialog
-                  open={reviewOpen}
-                  setReviewOpen={setReviewOpen}
-                />
-                <div className="clubRight">
-                  <div className="histContainer">
-                    <img className="clubHist"></img>
-                  </div>
+                <div className="col">
+                  <h1 className="clubTitle">Completely Fake Club</h1>
                 </div>
-                <div className="reviewContainer">
-
+                <div className="col">
+                  <img className="share"></img>
                 </div>
               </div>
-            </section>
-          </main>
-        </>
-      );
+              <div className="row">
+                <div className="col">
+                  <a href=""><i class="fa-brands fa-instagram-square"></i></a>
+                </div>
+                <div className="col">
+                  <a href=""><i class="fa-brands fa-facebook-square"></i></a>
+                </div>
+                <div className="col">
+                  <a href=""><i class="fa-solid fa-envelope-open-text"></i></a>
+                </div>
+                <div className="col">
+                  <a href=""><i class="fa-solid fa-code"></i></a>
+                </div>
+              </div>
+              <div className="row">
+                <div className="tags">
+                  <p>tag</p>
+                </div>
+                <div className="tags">
+                  <p>tag</p>
+                </div>
+              </div>
+              <p className="clubDescrip">A club description.</p>
+              <button className="rateClub" onClick={handleReview}>Rate this club!</button>
+            </div>
+            <ReviewDialog
+              open={reviewOpen}
+              setReviewOpen={setReviewOpen}
+            />
+            <div className="col clubRight">
+              <div className="histContainer">
+                <img className="clubHist"></img>
+              </div>
+            </div>
+            <div className="reviewContainer">
+              <h1>Reviews</h1>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-function reviewDialog(props) {
-  const { open, setReviewOpen} = props;
-
+function ReviewDialog(props) {
+  const { open, setReviewOpen } = props;
   return (
     <Dialog
       aria-labelledby="simple-dialog-title"
@@ -79,20 +89,16 @@ function reviewDialog(props) {
           <i class="fa fa-times" aria-hidden="true"></i>
         </button>
         <h1>Rate: Totally fake club</h1>
+        <p>Rate this club:</p>
         <form action="put" method="#">
           <section className="form">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email" />
-            <label for="password">Password</label>
-            <input type="password" placeholder="Password" />
-            <button className="btn btn-primary">Continue</button>
-          </section>
+            <label for="leaveReview">Write a review</label>
+            <input type="" name="" id="" placeholder="" />
+            </section>
         </form>
       </div>
     </Dialog>
   );
 }
-
-
 
 export default Club;
