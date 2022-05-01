7e601b324444fb691a14353104baca67b8e6341e
JustinBautista51 <45113874+JustinBautista51@users.noreply.github.com>
2022-05-01 00:39:36 -0400

    added histogram

--- ratemyclub/src/pages/club.jsx
+++ ratemyclub/src/pages/club.jsx
@@ -1,17 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, PureComponent } from "react";
 import { Dialog } from "@material-ui/core";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-
+import {
+  ComposedChart,
+  Line,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  LabelList,
+  ResponsiveContainer,
+} from 'recharts';
+let starAverage = 0;
+let starArr = []
 const Club = () => {
   const [reviewOpen, setReviewOpen] = useState(false);
   const [club, setClub] = useState([]);
   const [review, setReview] = useState([]);
-
   const { id } = useParams();
   const handleReview = (event) => {
     setReviewOpen(true);
   };
+
+
 
   const fetchClub = async () => {
     const { data } = await axios.get(
@@ -19,6 +33,10 @@ const Club = () => {
     );
     console.log(data);
     let clubData = filterClub(data, id)[0];
+    console.log(clubData.reviews)
+    let reviewTotal = average(clubData.reviews);
+    starArr = starNumber(clubData.reviews)
+    starAverage = reviewTotal
     console.log(clubData.name);
     if (clubData.reviews) {
       let reviewsArr = clubData.reviews;
@@ -26,6 +44,8 @@ const Club = () => {
       const arr = reviewsArr.map((element) => {
         return [element.review, element.stars, element.tags];
       });
+
+
       setReview(arr);
     }
     setClub([
@@ -45,12 +65,36 @@ const Club = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const data = [
+    {
+      name: 'Awesome',
+      Stars: starArr[4],
+    },
+    {
+      name: 'Great',
+      Stars: starArr[3],
+    },
+    {
+      name: 'Good',
+      Stars: starArr[2],
+    },
+    {
+      name: 'Okay',
+      Stars: starArr[1],
+    },
+    {
+      name: 'Needs work',
+      Stars: starArr[0],
+    },
+
+  ];
+
   return (
     <>
       <main id="club" className="py-4">
         <section className="container">
           <div className="row club">
-            <div>
+            <div className="col clubber">
               <div className="clubTop">
                 <img className="clubImg" src={club[4]}></img>
                 <h1 className="clubTitle">{club[0]}</h1>
@@ -80,9 +124,41 @@ const Club = () => {
                 className="rateClub btn btn-primary"
                 onClick={handleReview}
               >
-                Rate this club!
+                Rate this club!!!!!
               </button>
             </div>
+            <div className="col histogram">
+
+
+              <p id="Ssize">{starAverage}/5</p>
+              <ResponsiveContainer width="100%" height="100%">
+                <ComposedChart
+                  layout="vertical"
+                  width={1000}
+                  height={300}
+                  data={data}
+                  margin={{
+                    top: 2,
+                    right: 20,
+                    bottom: 100,
+                    left: 20,
+                  }}
+                >
+
+                  <XAxis type="number" stroke="#FFFF" />
+                  <YAxis dataKey="name" type="category" scale="band" />
+                  <Tooltip />
+
+
+
+                  <Bar dataKey="Stars" barSize={20} fill="#FFD43D"><LabelList dataKey="Stars" position="right" /></Bar>
+
+                </ComposedChart>
+              </ResponsiveContainer>
+
+            </div>
+          </div>
+          <div>
             <h3 className="mt-4 mb-3 fw-bold">Reviews</h3>
             {review.length > 0 &&
               review.map((element) => (
@@ -100,11 +176,7 @@ const Club = () => {
                 </div>
               ))}
             <ReviewDialog open={reviewOpen} setReviewOpen={setReviewOpen} />
-            <div className="col clubRight">
-              <div className="histContainer">
-                <img className="clubHist"></img>
-              </div>
-            </div>
+
             <div className="reviewContainer"></div>
           </div>
         </section>
@@ -147,5 +219,38 @@ const filterClub = (arr, searchKey) => {
     Object.keys(obj).some((key) => obj[key].includes(searchKey))
   );
 };
+const average = (review) => {
+  console.log(review)
+  let i = 0;
+  let count = 0;
+  for (let j = 0; j <= review.length - 1; j++) {
+    i += review[j].stars
+    count++
+  }
+  let ratio = i / count
+  return (Math.round(ratio * 10.0) / 10.0);
+}
+const starNumber = (review) => {
+  let starArray = [0, 0, 0, 0, 0]
+  for (let j = 0; j <= review.length - 1; j++) {
+    if (review[j].stars === 1) {
+      starArray[0]++
+    }
+    if (review[j].stars === 2) {
+      starArray[1]++
+    }
+    if (review[j].stars === 3) {
+      starArray[2]++
+    }
+    if (review[j].stars === 4) {
+      starArray[3]++
+    }
+    if (review[j].stars === 5) {
+      starArray[4]++
+    }
+  }
+  return starArray
+  //take in review, go to review[j] star, do check, update the count, return variables 
+}
 
 export default Club;
